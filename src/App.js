aaa2e3245c041175c46b0dc524facaf417229a13
Syed Faraz <syedfarazuddin73@gmail.com>
2023-08-09 08:53:13 +0530

    sbj interiors completed

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+// import UsersTable from "./Components/AdminDashboard/UsersTable";
+// import { Login } from "./Components/Login/Login";
+// import { Register } from "./Components/Register/Register";
+import Home from "./Components/Home/Home";
+
+// import AdminDashboard from "./Components/AdminDashboard/AdminDashboard";
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
